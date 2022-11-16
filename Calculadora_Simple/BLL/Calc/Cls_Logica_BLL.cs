e0fe7fdc9b928099723a22026a45ded686292d10
Álvaro Gómez <85979342+crimenvaro@users.noreply.github.com>
2022-11-15 18:22:19 -0600

    Add files via upload

---
+++ Calculadora_Simple/BLL/Calc/Cls_Logica_BLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Calc
+{
+    class Cls_Logica_BLL
+    {
+    }
+}
